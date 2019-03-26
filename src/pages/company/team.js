72d7ecd28c40f6b44f4a0619d9a837aee37d761f
dislersd <dislersd@gmail.com>
2019-03-25 22:29:01 -0700

    added new about page and company-team page

--- src/pages/company/team.js
+++ src/pages/company/team.js
@@ -1,10 +1,10 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../../components/layout"
 
-const About = () => (
+const Team = () => (
   <Layout>
     <div>
-      <h1>About Us </h1>
+      <h1>Team</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi optio explicabo voluptates impedit quidem possimus? Necessitatibus, quisquam. Sit aliquam officiis nemo nulla, debitis vel aperiam quos expedita, deserunt tempore facilis! Tenetur magnam molestiae commodi, modi reiciendis quia adipisci quasi vero repellat corrupti mollitia atque non id esse assumenda aspernatur dolorum.
       </p>
@@ -12,4 +12,4 @@ const About = () => (
   </Layout>
 )
 
-export default About;
+export default Team;
